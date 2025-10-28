f59f60a62058523b0505ebf322e47f502b12378f
Ryan Hastie <231561729+ryanhastie5890@users.noreply.github.com>
2025-10-28 14:14:49 -0400

    Rich Library (#4)
    * styled text with the rich library
    
    * added an exit message

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,8 +1,6 @@
 from adventure.utils import read_events_from_file
 import random
-
-default_message = "You stand still, unsure what to do. The forest swallows you."
-
+from rich import print
 def step(choice: str, events):
     random_event = random.choice(events)
 
@@ -11,22 +9,24 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return default_message
+        return "[blue][bold red]You[/bold red] stand still, unsure what to do. The forest [bold red]swallows[/bold red] you.[/blue]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[blue]You walk [bold red]left[/bold red]. " + event+"[/blue]\n"
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[blue]You walk [bold red]right[/bold red]. " + event+"[/blue]\n"
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[blue]You wake up in a dark forest. You can go [bold red]left or right[/bold red].[/blue]\n")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        print("[blue]Which direction do you choose? ([bold red]left/right/exit[/bold red]): [/blue]\n")
+        choice = input()
         choice = choice.strip().lower()
         if choice == 'exit':
+            print("[blue]Thank you for playing! [bold red]Good Bye![/bold red][/blue]")
             break
         
         print(step(choice, events))
